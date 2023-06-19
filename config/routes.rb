965ebaa803726814d2ae30d183d3cc64fbc6a78d
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-06-19 07:25:20 +0000

    [Add]admin-login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-#管理者用
   devise_for :admin,skip: [:registrations, :passwords], controllers: {
   sessions: "admin/sessions"
+}
+  devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
 }
 
   namespace :admin do
@@ -13,28 +16,22 @@ Rails.application.routes.draw do
     resources :orders_details, only: [:update]
   end
 
-#会員用
-  devise_for :customers,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
-
-    root to: "public/homes#top"
-    get "/about" => "public/homes#about"
-    resources :items, only: [:index, :show]
-    resource :customers, only: [:show, :update] do
-      get 'information/edit' => "customers#edit", on: :collection
-      get 'leave', on: :collection
-      patch 'withdrawal', on: :collection
-    end
-    resources :cart_items, only: [:index, :update, :destroy, :create] do
-      delete 'destroy_all', on: :collection
-    end
-    resources :orders, only: [:new, :create, :index, :show] do
-      post 'confirm', on: :collection
-      get 'completion', on: :collection
-    end
-    resources :deliveries, only: [:index, :edit, :create, :update, :destroy]
+  root to: "public/homes#top"
+  get "/about" => "public/homes#about"
+  resources :items, only: [:index, :show]
+  resource :customers, only: [:show, :update] do
+    get 'information/edit' => "customers#edit", on: :collection
+    get 'leave', on: :collection
+    patch 'withdrawal', on: :collection
+  end
+  resources :cart_items, only: [:index, :update, :destroy, :create] do
+    delete 'destroy_all', on: :collection
+  end
+  resources :orders, only: [:new, :create, :index, :show] do
+    post 'confirm', on: :collection
+    get 'completion', on: :collection
+  end
+  resources :deliveries, only: [:index, :edit, :create, :update, :destroy]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
