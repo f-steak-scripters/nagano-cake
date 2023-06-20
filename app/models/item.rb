0e5ce0266754674c9d87f1664c0ad3b318967001
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-06-20 07:51:17 +0000

    [Add]public-item(途中)

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,12 @@ class Item < ApplicationRecord
 	validates :genre_id, presence: true
 	validates :price, presence: true
 	validates :price, numericality: { only_integer: true }
+	
+	def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end
