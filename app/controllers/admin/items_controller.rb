e06032ff9040c6d04152bec0f50fdf2f5ef1f184
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-06-20 04:23:01 +0000

    [Add]admin/商品登録機能・商品一覧機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,11 +1,20 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
   
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
   
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_items_path
+    else
+      render :new
+    end
   end
   
   def show
@@ -16,4 +25,65 @@ class Admin::ItemsController < ApplicationController
   
   def update
   end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :genre_id, :price, :is_sales )
+  end
+  
 end
+
+
+
+
+
+
+#   before_action :set_product, only: [:show, :edit, :update]
+#   before_action :set_genres, only: [:new, :edit, :index, :create, :update]
+
+#   def new
+#     @item = Item.new
+#   end
+
+#   def create
+#     @item = Item.new(itemt_params)
+#     if @item.save
+#       flash[:notice] = "商品を登録しました"
+#       redirect_to admin_product_path(@product)
+#     else
+#       render :new
+#     end
+#   end
+
+#   def index
+#     @items = Item.all.page(params[:page]).per(10)
+#   end
+
+#   def show
+#   end
+
+#   def edit
+#   end
+
+#   def update
+#     if @item.update(item_params)
+#       flash[:success] = "商品内容をを変更しました"
+#       redirect_to admin_item_path(@item)
+#     else
+#       render :edit
+#     end
+#   end
+
+#   private
+
+#   def item_params
+#     params.require(:item).permit(:name, :image, :explanation,
+#       :genre_id, :price, :is_sales)
+#   end
+
+#   def set_genres
+#     @genres = Genre.where(is_valid: true)
+#   end
+  
+# end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  has_many :cart_items
+  has_many :order_details
+  belongs_to :genre
+  has_one_attached :image
+  
+  validates :name, presence: true
+	validates :description, presence: true
+	validates :genre_id, presence: true
+	validates :price, presence: true
+	validates :price, numericality: { only_integer: true }
 end
