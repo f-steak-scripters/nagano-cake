2a353d9fe1f3f1c54aa31cf44fd0ee2bf472ef23
kukunju <132256077+kukunju@users.noreply.github.com>
2023-06-21 05:11:59 +0000

    [Add]会員一覧　会員情報

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,13 +1,15 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customer = Customer.all
   end
-  
+
   def show
+    @customer = Customer.find(params[:id])
   end
-  
+
   def edit
   end
-  
+
   def update
   end
 end
