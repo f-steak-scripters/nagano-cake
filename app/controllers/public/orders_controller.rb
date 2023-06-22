004fb26862d0ec34943ffc889557ed4f00fd5e09
EC2 Default User <ec2-user@ip-172-31-25-102.ap-northeast-1.compute.internal>
2023-06-22 01:10:06 +0000

    [add]public-order-confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -2,15 +2,25 @@ class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
     @deliveries = current_customer.deliveries.all
+    @customer = current_customer
   end
 
   def confirm
+    #address_type = params[:order][:address_type].to_i ifで作る比較のときはiとs気をつける
+    @order = Order.new(order_params)
+  
   end
 
   def completion
   end
 
   def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to 'completion'
+    else
+      render 'new'
+    end
   end
 
   def index
@@ -18,4 +28,11 @@ class Public::OrdersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:method_of_payment, :customer_id, :post_code, :address, :name)
+  end
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :customer
-  has_many :order_details
+  has_many :order_details 
   enum method_of_payment:{ card:0,bank:1}
   enum status: { waiting: 0, paid_up: 1, making: 2, preparing: 3 ,shipped:4}
   
