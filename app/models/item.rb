597e4ac404471b05d6d6652afe2863827fc0be9d
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-06-23 04:45:39 +0000

    [Add]商品画像バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,13 @@ class Item < ApplicationRecord
   has_many :cart_items
   belongs_to :genre
   has_one_attached :image
-  
+
   validates :name, presence: true
 	validates :description, presence: true
 	validates :genre_id, presence: true
 	validates :price, presence: true
 	validates :price, numericality: { only_integer: true }
+	validates :image, presence: true
 
 
   def get_image(width, height)
