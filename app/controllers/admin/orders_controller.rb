9692dc04324bd623630e75a871d10d70fe2422e7
kukunju <132256077+kukunju@users.noreply.github.com>
2023-06-23 05:57:16 +0000

    [Add]管理者用注文履歴詳細

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,21 @@
 class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
+    @order_details = OrderDetail.where(order_id: @order.id)
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order.id)
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:method_of_payment, :customer_id, :post_code, :address, :name ,:postage ,:billing_amount,:status)
+  end
+
+
 
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,10 @@
 class OrderDetail < ApplicationRecord
-  enum status: { impossible: 0, waiting: 1, making: 2, completation: 3 }
+  enum status: {
+    '着手不可': 0,
+    '制作待ち': 1,
+    '制作中': 2,
+    '制作完了': 3
+  }
   belongs_to :item
   belongs_to :order
   def subtotal
